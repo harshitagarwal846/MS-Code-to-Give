711b7059339e110f496954c34d5f0df483e65cb5
Digvijay Anand <99537348+digvijayyanand@users.noreply.github.com>
2023-06-06 17:13:12 +0530

    server and basic page combined

---
+++ combined/routes/auth.route.js
@@ -0,0 +1,24 @@
+
+
+const express = require("express");
+const router = express.Router();
+
+
+const checkAuth = require("../middlewares/checkAuth");
+const checkAdmin = require("../middlewares/checkAdmin");
+const {
+    createNewUser, handleAdmin, loginWithPhoneOtp, fetchCurrentUser, verifyPhoneOtp
+} = require("../controllers/auth.controller");
+
+
+
+router.post("/login_with_phone", loginWithPhoneOtp);
+router.post("/register", createNewUser)
+
+router.post("/verify", verifyPhoneOtp);
+
+router.get("/me", checkAuth, fetchCurrentUser);
+
+router.get("/admin", checkAuth, checkAdmin, handleAdmin);
+
+module.exports = router;
