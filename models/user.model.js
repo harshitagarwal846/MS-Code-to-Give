8ca5e5c00787c2ac7ae1fc6f0cd87cd38549056c
RANAPRATHAP <87611453+Ranavicky@users.noreply.github.com>
2023-06-10 09:25:54 +0530

    Update user.model.js
    Added extra fields in user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,31 +1,41 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  phoneNumber: {
-    type: String,
-    required: true
-  },
-  college: {
-    type: String,
-    required: true
-  },
-  gender: {
-    type: String,
-    enum: ['Male', 'Female', 'Other'],
-    required: true
-  },
-  hasBeenToCounseling: {
-    type: Boolean,
-    required: true
-  }
+ name: {
+   type: String,
+   required: true
+ },
+ phoneNumber: {
+   type: String,
+   required: true
+ },
+ college: {
+   type: String,
+   required: true
+ },
+ gender: {
+   type: String,
+   enum: ['Male', 'Female', 'Other'],
+   required: true
+ },
+ hasBeenToCounseling: {
+   type: Boolean,
+   required: true
+ },
+ hasBeenToRehabilitation: {
+   type: Boolean,
+   required: true
+ },
+ questionnaireId: {
+   type: mongoose.Schema.Types.ObjectId,
+   required: true
+ },
+ password: {
+   type: String,
+   required: true
+ }
 });
 
 const User = mongoose.model('User', userSchema);
-
-module.exports = User;
 
 module.exports = User;
